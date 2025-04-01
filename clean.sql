c54263133451474fff01f11e30cd408c0ca9e310
Kendall Leonard <89801130+k-leonard@users.noreply.github.com>
2025-03-31 20:13:15 -0700

    Update clean.sql

--- clean.sql
+++ clean.sql
@@ -82,9 +82,80 @@ CAST(google_movies.raw_json->>2 AS INT) AS points,
 FROM google_movies
 WHERE google_movies.raw_json IS NOT NULL;
 
+INSERT INTO Rakuten_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (rakuten_movies.raw_json ->>1, rakuten_movies.added_at)
+(rakuten_movies.raw_json->>0) AS rank,
+(rakuten_movies.raw_json->>1) AS title,
+CAST(rakuten_movies.raw_json->>2 AS INT) AS points,
+(rakuten_movies.added_at) AS date_scraped
+FROM rakuten_movies
+WHERE rakuten_movies.raw_json IS NOT NULL;
+
+INSERT INTO Chili_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (chili_movies.raw_json ->>1, chili_movies.added_at)
+(chili_movies.raw_json->>0) AS rank,
+(chili_movies.raw_json->>1) AS title,
+CAST(chili_movies.raw_json->>2 AS INT) AS points,
+(chili_movies.added_at) AS date_scraped
+FROM chili_movies
+WHERE chili_movies.raw_json IS NOT NULL;
+
+INSERT INTO Vudu_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (vudu_movies.raw_json ->>1, vudu_movies.added_at)
+(vudu_movies.raw_json->>0) AS rank,
+(vudu_movies.raw_json->>1) AS title,
+CAST(vudu_movies.raw_json->>2 AS INT) AS points,
+(vudu_movies.added_at) AS date_scraped
+FROM vudu_movies
+WHERE vudu_movies.raw_json IS NOT NULL;
+
+INSERT INTO freevee_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (freevee_movies.raw_json ->>1, freevee_movies.added_at)
+(freevee_movies.raw_json->>0) AS rank,
+(freevee_movies.raw_json->>1) AS title,
+CAST(freevee_movies.raw_json->>2 AS INT) AS points,
+(freevee_movies.added_at) AS date_scraped
+FROM freevee_movies
+WHERE freevee_movies.raw_json IS NOT NULL;
 
 
+INSERT INTO osn_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (osn_movies.raw_json ->>1, osn_movies.added_at)
+(osn_movies.raw_json->>0) AS rank,
+(osn_movies.raw_json->>1) AS title,
+CAST(osn_movies.raw_json->>2 AS INT) AS points,
+(osn_movies.added_at) AS date_scraped
+FROM osn_movies
+WHERE osn_movies.raw_json IS NOT NULL;
 
+
+INSERT INTO Starz_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (starz_movies.raw_json ->>1, starz_movies.added_at)
+(starz_movies.raw_json->>0) AS rank,
+(starz_movies.raw_json->>1) AS title,
+CAST(starz_movies.raw_json->>2 AS INT) AS points,
+(starz_movies.added_at) AS date_scraped
+FROM starz_movies
+WHERE starz_movies.raw_json IS NOT NULL;
+
+
+INSERT INTO Viaplay_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (viaplay_movies.raw_json ->>1, viaplay_movies.added_at)
+(viaplay_movies.raw_json->>0) AS rank,
+(viaplay_movies.raw_json->>1) AS title,
+CAST(viaplay_movies.raw_json->>2 AS INT) AS points,
+(viaplay_movies.added_at) AS date_scraped
+FROM viaplay_movies
+WHERE viaplay_movies.raw_json IS NOT NULL;
+
+INSERT INTO Hulu_Movie_Rankings(rank, title, points, date_scraped)
+SELECT DISTINCT ON (hulu_movies.raw_json ->>1, hulu_movies.added_at)
+(hulu_movies.raw_json->>0) AS rank,
+(hulu_movies.raw_json->>1) AS title,
+CAST(hulu_movies.raw_json->>2 AS INT) AS points,
+(hulu_movies.added_at) AS date_scraped
+FROM hulu_movies
+WHERE hulu_movies.raw_json IS NOT NULL;
 
 
 COMMIT;
